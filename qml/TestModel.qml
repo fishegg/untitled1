30cc3266b04e7d6dcbba1204c9f0440adbc739c8
fishegg <fishball123@163.com>
2017-03-18 20:29:49 +0800

    65315

---
+++ qml/TestModel.qml
@@ -0,0 +1,25 @@
+import QtQuick 2.0
+
+ListModel　{
+
+    ListElement {
+        line: "L1"
+        station_number: "101"
+        station: "金沙洲码头"
+    }
+    ListElement {
+        line: "L1"
+        station_number: "102"
+        station: "黄沙码头"
+    }
+    ListElement {
+        line: "L1"
+        station_number: "103"
+        station: "西堤码头"
+    }
+    ListElement {
+        line: "L2"
+        station_number: "201"
+        station: "芳村码头"
+    }
+}
---
+++ translations/untitled1.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>My Cover</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Show Page 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>from</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>to</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SecondPage</name>
+    <message>
+        <source>Nested Page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Item</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
